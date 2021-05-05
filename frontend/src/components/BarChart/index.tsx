6cd1a38f43a5a2ac52d62b715f74c4322764aa6f
Mateus Silva <theusskt2018@gmail.com>
2021-05-05 10:32:31 -0300

    Security config

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -26,6 +26,8 @@ const BarChart = () => {
        <Chart 
           options={{ ...options, xaxis: mockData.labels}}
           series={mockData.series}
+          type="bar"
+          height="240"
        />
     );
 }
